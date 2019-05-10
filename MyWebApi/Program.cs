f871f5aea0542ae59de86b0a96cdab3c89241051
mirror <mirror010@hotmail.com>
2019-05-10 19:15:35 +0800

    建立

---
+++ MyWebApi/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 主机功能配置
+            void serverCfg(IServiceCollection service)
+            {
+                // 跨域功能
+                service.AddCors();
+            }
+
+            // 跨域策略配置
+            void cors(CorsPolicyBuilder cfg)
+            {
+                cfg.AllowAnyHeader();
+                cfg.AllowAnyMethod();
+                cfg.AllowAnyOrigin();
+                cfg.AllowCredentials();
+            }
+
+            // kestrel服务器配置
+            IConfiguration kestrelCfg = new ConfigurationBuilder()
+                .AddJsonFile("kestrel.json")
+                .Build();
+
+            // 开机运行
+            new WebHostBuilder()
+                .ConfigureServices(serverCfg)
+                .UseConfiguration(kestrelCfg)
+                .UseKestrel()
+                .Configure(app => app
+                    .UseCors(cors)
+                    .UseDeveloperExceptionPage()
+                    .Use(ApiHandler.UrlHandler)
+                )
+                .Build()
+                .Run();
+                //.RunAsService();
+        }
+    }
+}
---
+++ MyWebApi/test/Demo1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebApi.api
+{
+    class Demo1Api:ApiBase
+    {
+        [HTTPGET]
+        public async Task index()
+        {
+            var res = new
+            {
+                name = "url地址:api/demo1/index",
+                info = "api是命名空间,后面是类名和方法名"
+            };
+            await this.Json(res);
+        }
+    }
+}
