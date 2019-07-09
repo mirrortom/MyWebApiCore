db1daaa6bd292b384507bb690708a9bbfb31f4a4
mirror <mirror010@hotmail.com>
2019-07-09 18:10:53 +0800

    注释说明

--- MyWebApi/Program.cs
+++ MyWebApi/Program.cs
@@ -4,9 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MyWebApi
 {
@@ -14,14 +11,14 @@ namespace MyWebApi
     {
         static void Main(string[] args)
         {
-            // 主机功能配置
+            // 主机功能配置项
             void serverCfg(IServiceCollection service)
             {
                 // 跨域功能
                 service.AddCors();
             }
 
-            // 跨域策略配置
+            // 跨域策略配置项
             void cors(CorsPolicyBuilder cfg)
             {
                 cfg.AllowAnyHeader();
@@ -30,20 +27,21 @@ namespace MyWebApi
                 cfg.AllowCredentials();
             }
 
-            // kestrel服务器配置
+            // kestrel服务器配置文件载入
             IConfiguration kestrelCfg = new ConfigurationBuilder()
                 .AddJsonFile("kestrel.json")
                 .Build();
 
             // 开机运行,可选择其中一种方式运行,服务或者控制台
+            // 实例化主机,载入配置项
             IWebHost webhost = new WebHostBuilder()
                 .ConfigureServices(serverCfg)
                 .UseConfiguration(kestrelCfg)
                 .UseKestrel()
                 .Configure(app => app
-                    .UseCors(cors)
-                    .UseDeveloperExceptionPage()
-                    .Use(ApiHandler.UrlHandler)
+                    .UseCors(cors)// 跨域
+                    .UseDeveloperExceptionPage()// 能在请求页面上显示异常信息
+                    .Use(ApiHandler.UrlHandler)// 自定义路由中间件
                 )
                 .Build();
             if (args.Length > 0 && args[0] == "s")
